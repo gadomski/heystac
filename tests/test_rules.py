cdf659ab699d2a77a7d1fe63324fdbb281366341
Pete Gadomski <pete.gadomski@gmail.com>
2025-05-08 08:49:42 -0600

    feat: initial commit, bringing over from frontend

---
+++ tests/test_rules.py
@@ -0,0 +1,14 @@
+from heystac import Catalog, Context, rules
+
+
+def test_validate_core(root: Catalog) -> None:
+    context = Context()
+
+    check = rules.validate_core(context, root)
+    assert check
+    assert check.score == 1
+
+    root.stac_version = "not-a-real-version"
+    check = rules.validate_core(context, root)
+    assert check
+    assert check.score == 0
